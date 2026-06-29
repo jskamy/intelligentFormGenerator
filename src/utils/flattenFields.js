eceeffa2882405e398c244a21369f094480e52d4
jskamy <jsakademy@gmail.com>
2026-06-29 21:58:49 +0530

    flatten file desc

--- src/utils/flattenFields.js
+++ src/utils/flattenFields.js
@@ -1,3 +1,5 @@
+//This file is used to flatten the incoming param into a single level array - fields only
+
 export default function flattenFields(sections){
     return sections.flatMap(section => section.fields)
 }
